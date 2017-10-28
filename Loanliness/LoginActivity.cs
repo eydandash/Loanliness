d5ce37b911279c9066c87e71a2552577ee63c4b3
Sebastian Sas <ssasmangel1@sheffield.ac.uk>
2017-10-28 04:05:15 +0100

    Initial commit. Login screen started

---
+++ Loanliness/LoginActivity.cs
@@ -0,0 +1,59 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Support.V7.App;
+using Firebase.Auth;
+using Android.Gms.Tasks;
+
+namespace Loanliness
+{
+    [Activity(Label = "LogIn",MainLauncher = true,Theme = "@style/Theme.AppCompat.Light.NoActionBar")]
+    public class LoginActivity : AppCompatActivity, IOnCompleteListener
+    {
+        FirebaseAuth auth;
+
+        public void OnComplete(Task task)
+        {
+            if(task.IsSuccessful)
+            {
+                Toast.MakeText(this, "Registered Successfully!", ToastLength.Short).Show();
+                Finish();
+            }
+            else
+            {
+                Toast.MakeText(this, "Registration Failed! Please try again.", ToastLength.Short).Show();
+                Finish();
+            }
+        }
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+            SetContentView(Resource.Layout.Login);
+
+            var mEmail = FindViewById<EditText>(Resource.Id.txtEmailLogIn);
+            var mPass = FindViewById<EditText>(Resource.Id.txtPassLogIn);
+
+            var mBtnReg = FindViewById<Button>(Resource.Id.btnRegisterLogIn);
+            var mBtnSignIn = FindViewById<Button>(Resource.Id.btnSignInLogIn);
+
+            mBtnReg.Click += delegate {
+                auth.CreateUserWithEmailAndPassword(mEmail.Text, mPass.Text)
+                    .AddOnCompleteListener(this);
+            };
+
+
+        }
+    }
+}
---
+++ Loanliness/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace Loanliness
+{
+    [Activity(Label = "Loanliness", Icon = "@mipmap/icon")]
+    public class MainActivity : Activity
+    {
+        int count = 1;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+        }
+    }
+}
+
